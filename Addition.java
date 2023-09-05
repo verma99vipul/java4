fb554ab530c715c4a407e7d93bf048a5bb93bf61
verma99vipul <v99vipul@gmail.com>
2023-09-05 12:26:33 +0530

    java code added

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition{
+	int num1= 2;
+	int num2= 3;
+	int num3= 6;
+	int sum= num1+num2+num3;
+	
+public static void main(String[] args){
+	
+
+	Addition obj = new Addition();
+
+System.out.println(obj.sum);
+}
